f16fc6f588ff655b19d5f3495c9191db016b19bc
jasonbrodys <srirams.cud@gmail.com>
2020-12-21 12:53:15 +0530

    add2

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello();
-    retutn "<h1>Hello World!</h1>"
+def hello():
+    return "<h1>Hey I am Arun Raj Date= 21-12-2020</h1>"
 
-     if __name__== '__main__':
-        app.run(port=5000,  threaded=true)
+if __name__ == '__main__':
+    app.run(port=5000, threaded=True)
